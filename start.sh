7b9b5e82e9e9ff2d38ad0fd5828060a5ea6dbead
wr-lancel <Lancel.Mangio@worldready.ai>
2026-03-10 23:20:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -61,6 +61,10 @@ rm -f /etc/apache2/mods-enabled/mpm_*.conf
 ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
 ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
 
+# Start the Laravel queue worker in the background to handle emails
+echo "Starting Laravel Queue Worker..."
+php artisan queue:work --database=mysql --sleep=3 --tries=3 &
+
 # Start the Apache server in the foreground
 echo "Starting Apache server..."
 apache2ctl -t
